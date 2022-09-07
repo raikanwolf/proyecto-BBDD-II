22859f93cba1c484164bd2fa69685239f5c8b94e
raikanwolf <helvis.eloy1998@gmail.com>
2022-09-07 19:22:43 -0400

    intentando poner vue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,19 +2,22 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>LaraDex-@yield('title')</title>
+	<title>JGtravels-@yield('title')</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
 </head>
 <body>
 	<nav class="navbar navbar-dark bg-dark">
-		<a href="#" class="navbar-brand">Laradex</a>
-		<a href="#" class="navbar-brand">Entrenadores</a>
-		<a href="#" class="navbar-brand">Crear</a>
+		<a href="#" class="navbar-brand">enlace</a>
+		<a href="#" class="navbar-brand">enlace</a>
+		<a href="#" class="navbar-brand">enlace</a>
 	</nav>
-	<div class="container">
-		@yield('content')	
+	<div id="app" class="container">
+		@yield('content')
+		<otro-ejemplo></otro-ejemplo>
+		
 	</div>
 	
+	<script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,5 @@
 @extends('layouts.app')
 @section('title','Index')
+@section('content')
+    <p style="color: #fff;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda repellat dignissimos, consequatur iure necessitatibus nisi quasi praesentium, sapiente, sequi quod natus. Autem voluptas, molestias! Fuga provident, delectus voluptas rem. Dolor!</p>
+@endsection
